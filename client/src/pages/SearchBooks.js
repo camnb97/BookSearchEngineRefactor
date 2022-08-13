3b7915116bb590905684d80e6a9209c5f0fe3b3f
Cameron Buss <camnb97@gmail.com>
2022-08-12 18:37:29 -0700

    fixed searchbooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -3,8 +3,8 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 
 import Auth from '../utils/auth';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-import { SAVE_BOOK } from '../utils/mutations'
-import { useMutation } from '@apollo/client'
+import { SAVE_BOOK } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const SearchBooks = () => {
 
